6da04ed241ceff1c023911ccd70e6b836e04d442
M@ <matt@withani.net>
2020-05-11 21:29:34 -0700

    Fixed indents.

--- scripts/index.js
+++ scripts/index.js
@@ -14,67 +14,62 @@ window.onload=function() {
 
 	$(document).ready(function(){
 	// for each content class
-	$(".content").each(function() {
-		// check if our mouse is over the div
-		$(this).mouseover(function (){
-			// if the wheel scrolls
-			$(this).on('wheel', function (e){
+		$(".content").each(function() {
+			// check if our mouse is over the div
+			$(this).mouseover(function (){
+				// if the wheel scrolls
+				$(this).on('wheel', function (e){
 
-				// get the direction of the wheel scroll
-				var delta = e.originalEvent.deltaY;
+					// get the direction of the wheel scroll
+					var delta = e.originalEvent.deltaY;
 
-				// scroll down
-				if (delta > 0) {
-					// check if we are on the last div
-					// if we are we should not continue to scroll
-					if ($(this).next().length != 0) {
-						// slide the active div up to reveal the next div
-						$(this).css('transform', 'translateY(-100%)');
-					}
-				// scroll up
-				} else {
-					// find the previous div and slide it down on top of current
-					$(this).prev().css('transform', 'translateY(0%)');
-				}		
-			})
-		})
-
-		// check if we have a touch event over the div
-		$(this).on('touchstart', function(e) {
-			// get where the touch started
-			var touchStart = e.originalEvent.touches,
-			start = touchStart[0].pageY;
-
-			$(this).on('touchmove', function(e) {
-				// get where the touch moved to
-				var contact = e.originalEvent.touches,
-				end = contact[0].pageY,
-				distance = end-start;
-
-				// swipe up
-				if (distance < -30) {
-					// check if we are on the last div
-					// if we are we should not continue to scroll
-					if ($(this).next().length != 0) {
-						// slide the active div up to reveal the next div
-						$(this).css('transform', 'translateY(-100%)');
-					}
-				}
-				//swipe down
-				if (distance > 30) {
-					// find the previous div and slide it down on top of current
-					$(this).prev().css('transform', 'translateY(0%)');
-				}
-			}).on('touchend', function() {
-				$(this).off('touchmove touchend')
+					// scroll down
+					if (delta > 0) {
+						// check if we are on the last div
+						// if we are we should not continue to scroll
+						if ($(this).next().length != 0) {
+							// slide the active div up to reveal the next div
+							$(this).css('transform', 'translateY(-100%)');
+						}
+					// scroll up
+					} else {
+						// find the previous div and slide it down on top of current
+						$(this).prev().css('transform', 'translateY(0%)');
+					}		
+				})
 			})
 
+			// check if we have a touch event over the div
+			$(this).on('touchstart', function(e) {
+				// get where the touch started
+				var touchStart = e.originalEvent.touches,
+				start = touchStart[0].pageY;
+
+				$(this).on('touchmove', function(e) {
+					// get where the touch moved to
+					var contact = e.originalEvent.touches,
+					end = contact[0].pageY,
+					distance = end-start;
+
+					// swipe up
+					if (distance < -30) {
+						// check if we are on the last div
+						// if we are we should not continue to scroll
+						if ($(this).next().length != 0) {
+							// slide the active div up to reveal the next div
+							$(this).css('transform', 'translateY(-100%)');
+						}
+					}
+					//swipe down
+					if (distance > 30) {
+						// find the previous div and slide it down on top of current
+						$(this).prev().css('transform', 'translateY(0%)');
+					}
+				}).on('touchend', function() {
+					$(this).off('touchmove touchend')
+				})
+
+			})
 		})
 	})
-})
-
 }
-
-
-
-//console.log(document.getElementsByClassName("content"));
